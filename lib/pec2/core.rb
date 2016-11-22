40015f84e93ec16c7906725af2bc07de0b0096e3
toyama0919 <toyama0919@gmail.com>
2016-11-22 19:17:54 +0900

    fix auto get region.

--- lib/pec2/core.rb
+++ lib/pec2/core.rb
@@ -1,12 +1,18 @@
 require 'aws-sdk'
 require 'hashie'
 require 'logger'
+require 'open-uri'
+require 'json'
+require 'timeout'
+
 
 module Pec2
   class Core
+    TIME_OUT = 3
 
     def initialize
       @logger = Logger.new(STDOUT)
+      ENV['AWS_REGION'] = ENV['AWS_REGION'] || get_document['region']
       @ec2 = Aws::EC2::Client.new
     end
 
@@ -19,6 +25,23 @@ module Pec2
       @ec2.describe_instances(
         filters: filter
       ).data.to_h[:reservations].map { |instance| Hashie::Mash.new(instance[:instances].first) }
+    end
+
+    def get_document
+      JSON.parse(get_metadata('/latest/dynamic/instance-identity/document/'))
+    end
+
+    def get_metadata(path)
+      begin
+        result = {}
+        Timeout.timeout(TIME_OUT) {
+          body = open('http://169.254.169.254' + path).read
+          return body
+        }
+        return result
+      rescue Timeout::Error => e
+        raise "not EC2 instance"
+      end
     end
   end
 end
