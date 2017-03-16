e9ee168889c27c89c3673a7729710057ec728502
toyama0919 <toyama0919@gmail.com>
2017-03-16 14:48:45 +0900

    refactoring.

--- lib/pec2/cli.rb
+++ lib/pec2/cli.rb
@@ -26,42 +26,46 @@ module Pec2
     option :parallel, aliases: '-p', type: :numeric, desc: 'parallel'
     option :print, aliases: '-P', type: :boolean, default: false, desc: 'print stdout.'
     def search_tag
-      Tempfile.create("pec2") do |f|
-        addresses = @core.instances_hash(options[:tag]).map do |instance|
-          instance.private_ip_address
-        end
-
-        if addresses.empty?
-          @logger.error(%Q{no host tag #{options[:tag]}.})
-          raise
-        end
-
-        File.write(f.path, addresses.join("\n"))
-        cmd = "#{@pssh_path} -t 0 -x '-tt' -h #{f.path} -O StrictHostKeyChecking=no"
-
-        if options[:print]
-          cmd = "#{cmd} -P"
-        end
-
-        if options[:user]
-          cmd = "#{cmd} -l #{options[:user]}"
-        end
-
-        if options[:log]
-          cmd = "#{cmd} -o #{options[:log]}"
-        end
-
-        if options[:parallel]
-          cmd = "#{cmd} -p #{options[:parallel]}"
-        end
-
-        if options[:sudo_password]
-          cmd = %Q{(echo #{options[:sudo_password]}) | #{cmd} -I #{Shellwords.escape(options[:command])}}
-        else
-          cmd = %Q{#{cmd} -i #{Shellwords.escape(options[:command])}}
-        end
-        system(cmd)
+      cmd = ""
+      addresses = @core.instances_hash(options[:tag]).map do |instance|
+        instance.private_ip_address
       end
+
+      if addresses.empty?
+        @logger.error(%Q{no host tag #{options[:tag]}.})
+        raise
+      end
+
+      tf = Tempfile.open("pec2") { |fp|
+        fp.puts(addresses.join("\n"))
+        fp
+      }
+
+      cmd = "#{@pssh_path} -t 0 -x '-tt' -h #{tf.path} -O StrictHostKeyChecking=no"
+      if options[:print]
+        cmd = "#{cmd} -P"
+      end
+
+      if options[:user]
+        cmd = "#{cmd} -l #{options[:user]}"
+      end
+
+      if options[:log]
+        cmd = "#{cmd} -o #{options[:log]}"
+      end
+
+      if options[:parallel]
+        cmd = "#{cmd} -p #{options[:parallel]}"
+      end
+
+      if options[:sudo_password]
+        cmd = %Q{(echo #{options[:sudo_password]}) | #{cmd} -I #{Shellwords.escape(options[:command])}}
+      else
+        cmd = %Q{#{cmd} -i #{Shellwords.escape(options[:command])}}
+      end
+
+      system(cmd)
+      tf.close
     end
 
     desc 'version', 'show version'
